223d1703ca59dc836b8d397c99b0a45f58d7e226
yashodgayashan <yashodgayashan@gmail.com>
2020-03-22 22:14:55 +0530

    Created Login page

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: Text(
+        "Hi",
+        textDirection: TextDirection.ltr,
+      )),
+    );
+  }
+}
