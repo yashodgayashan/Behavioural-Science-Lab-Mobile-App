caea9a269087c16baccd72dc47266bb89e9a293e
yashodgayashan <yashodgayashan@gmail.com>
2020-03-25 10:04:10 +0530

    Added form validator

--- lib/screens/create_project.dart
+++ lib/screens/create_project.dart
@@ -10,89 +10,93 @@ class _SignUpState extends State<SignUp> {
   TextEditingController projectDescriptionController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
+  var _ProjectCreateformKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: ListView(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  "Create a Project",
-                  textScaleFactor: 2.0,
+      body: Form(
+        key: _ProjectCreateformKey,
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Create a Project",
+                textScaleFactor: 2.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: projectNameController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Please enter the project name";
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Project Name',
                 ),
               ),
-              Padding(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: projectDescriptionController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: passwordController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Please enter a password";
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: confirmPasswordController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Please enter a password";
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: projectNameController,
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return "Please enter the project name";
+                child: RaisedButton(
+                  onPressed: () {setState(() {
+                    if (_ProjectCreateformKey.currentState.validate()) {
+                      debugPrint("Okay");
                     }
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Project Name',
-                  ),
+                  });},
+                  child: Text("Next"),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: projectDescriptionController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Description',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: passwordController,
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return "Please enter a password";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: confirmPasswordController,
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return "Please enter a password";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text("Next"),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
