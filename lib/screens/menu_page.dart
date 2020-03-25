11f7cabddcc7cdd8d56e14571476f0c0ee39c29e
yashodgayashan <yashodgayashan@gmail.com>
2020-03-25 15:51:14 +0530

    Added tab controller for menu page

--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -8,9 +8,31 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Text("hi"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text("Project Overview",textAlign: TextAlign.center,)),
+              Tab(child: Text("Results")),
+              Tab(child: Text("Settings"),)
+            ],
+          ),
+          title: Text('Project Name'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.settings)
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
