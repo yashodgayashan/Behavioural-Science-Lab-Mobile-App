221c93704e720d731a42c9c52887c82a4159d317
yashodgayashan <yashodgayashan@gmail.com>
2020-03-26 16:26:11 +0530

    Added drag and drop list

--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -46,7 +46,8 @@ class _MenuState extends State<Menu> {
     return Column(
       children: <Widget>[
         durationWidget(context),
-
+        Text("Images"),
+        Expanded(child: drag(context))
       ],
     );
   }
@@ -59,5 +60,54 @@ class _MenuState extends State<Menu> {
     );
   }
 
+  List<String> _list = [
+    "Apple",
+    "Ball",
+    "Cat",
+    "Dog",
+    "Elephant",
+    "Goal",
+    "Ink",
+    "High",
+    "Row",
+    "COntainer"
+  ];
 
+  Widget drag(BuildContext context) {
+    return ReorderableListView(
+      scrollDirection: Axis.vertical,
+      children: _list
+          .map((item) => ListTile(
+                key: Key("${item}"),
+                title: Text("${item}"),
+
+              ))
+          .toList(),
+      onReorder: (int start, int current) {
+        // dragging from top to bottom
+        if (start < current) {
+          int end = current - 1;
+          String startItem = _list[start];
+          int i = 0;
+          int local = start;
+          do {
+            _list[local] = _list[++local];
+            i++;
+          } while (i < end - start);
+          _list[end] = startItem;
+        }
+        // dragging from bottom to top
+        else if (start > current) {
+          String startItem = _list[start];
+          for (int i = start; i > current; i--) {
+            _list[i] = _list[i - 1];
+          }
+          _list[current] = startItem;
+        }
+        setState(() {
+          debugPrint(_list.toString());
+        });
+      },
+    );
+  }
 }
